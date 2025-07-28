de9e8572b270f39040c16d504ff89cf947db3f76
aviatam-mend <149406890+aviatam-mend@users.noreply.github.com>
2025-07-28 16:29:15 +0300

    Add files via upload

---
+++ TufiApplication.java
@@ -0,0 +1,12 @@
+package com.tufi.secretmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TufiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TufiApplication.class, args);
+    }
+}
